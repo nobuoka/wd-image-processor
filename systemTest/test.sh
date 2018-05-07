7dd16a58affc26191884f6a958167c3b16947faa
Nobuoka Yu <nobuoka@vividcode.info>
2018-05-08 02:17:57 +0900

    Fix system test

--- systemTest/test.sh
+++ systemTest/test.sh
@@ -4,6 +4,6 @@ set -xeu
 
 base_url=${TEST_BASE_URL:-"http://localhost:8080"}
 
-status_code=`curl -s -o /dev/null -w '%{http_code}' $base_url/-/health`
+status_code=`curl -s -o /dev/null -w '%{http_code}' $base_url/-/health/all`
 echo "status code: $status_code"
 test $status_code -eq 200
