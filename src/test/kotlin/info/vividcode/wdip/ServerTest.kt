192219eed32480cf4b85cd37a614cc954301541e
Nobuoka Yu <nobuoka@vividcode.info>
2018-10-07 05:00:00 +0900

    Add test in case that request is not handled

--- src/test/kotlin/info/vividcode/wdip/ServerTest.kt
+++ src/test/kotlin/info/vividcode/wdip/ServerTest.kt
@@ -3,6 +3,8 @@ package info.vividcode.wdip
 import info.vividcode.wd.Timeouts
 import info.vividcode.wdip.application.WebDriverConnectionManager
 import io.ktor.http.Headers
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.withTestApplication
 import okhttp3.OkHttpClient
 import org.junit.jupiter.api.Assertions
@@ -10,15 +12,21 @@ import org.junit.jupiter.api.Test
 
 internal class ServerTest {
 
-    @Test
-    internal fun healthCheck() = withTestApplication {
+    private fun <T> withWdipApplicationModule(test: TestApplicationEngine.() -> T) = withTestApplication {
         val config = WdipSetting(emptySet(), emptyList())
         val wdSessionManager = WebDriverConnectionManager(OkHttpClient(), listOf("http://test.com/wd"), 1, Timeouts(1, 1, 1))
         application.setup(config, wdSessionManager, emptyList())
 
+        test()
+    }
+
+    @Test
+    internal fun healthCheck() = withWdipApplicationModule {
         handleRequest {
             uri = "/-/health/app-only"
         }.let { call ->
+            Assertions.assertTrue(call.requestHandled)
+            Assertions.assertEquals(HttpStatusCode.OK, call.response.status())
             Assertions.assertEquals(
                     Headers.build {
                         set("Vary", "Origin")
@@ -29,6 +37,23 @@ internal class ServerTest {
                     call.response.headers.allValues()
             )
             Assertions.assertEquals("OK", call.response.content)
+        }
+    }
+
+    @Test
+    internal fun notFound() = withWdipApplicationModule {
+        handleRequest {
+            uri = "/not-found"
+        }.let { call ->
+            Assertions.assertFalse(call.requestHandled)
+            Assertions.assertNull(call.response.status())
+            Assertions.assertEquals(
+                    Headers.build {
+                        set("Vary", "Origin")
+                        set("X-Content-Type-Options", "nosniff")
+                    },
+                    call.response.headers.allValues()
+            )
         }
     }
 
