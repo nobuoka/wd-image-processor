fc9f4c6049614b1716aa4665a4c9a98dec1ceaa0
Nobuoka Yu <nobuoka@vividcode.info>
2018-11-07 01:35:41 +0900

    Remove unused dependencies from wdip-usecase module

--- modules/wdip-usecase/build.gradle.kts
+++ modules/wdip-usecase/build.gradle.kts
@@ -24,22 +24,19 @@ val test by tasks.existing(Test::class) {
 }
 
 dependencies {
-    val okHttpVersion = "3.9.1"
     val kotlinxCoroutinesVersion = "0.25.0"
     val klaxonVersion = "2.1.4"
     val junitJupiterVersion = "5.2.0"
 
     implementation(project(":modules:wd"))
-    implementation(project(":modules:wd-okhttp"))
 
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
     implementation("com.beust:klaxon:$klaxonVersion")
 
-    implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
-    implementation("com.squareup.okhttp3:logging-interceptor:$okHttpVersion")
-
     // JUnit Jupiter API and TestEngine implementation
     testCompileOnly("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+
+    testImplementation(project(":modules:wd-okhttp"))
 }
