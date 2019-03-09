a4cef44103e3a8e034cb6903ea0a0621dfaf6e2a
Nobuoka Yu <nobuoka@vividcode.info>
2019-03-09 11:33:56 +0900

    Add test of `WdImageProcessingEndpoint`

--- modules/wdip-web/build.gradle.kts
+++ modules/wdip-web/build.gradle.kts
@@ -3,6 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm")
     `java-library`
+    id("jacoco")
+}
+
+jacoco {
+    toolVersion = "0.8.2"
 }
 
 tasks.withType<KotlinCompile> {
@@ -25,4 +30,12 @@ dependencies {
     // JUnit Jupiter API and TestEngine implementation
     testCompileOnly("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+}
+
+val jacocoTestReport by tasks.existing(JacocoReport::class) {
+    reports {
+        xml.isEnabled = true
+        xml.destination = file("$buildDir/reports/jacoco/report.xml")
+        html.destination = file("$buildDir/reports/jacoco/html")
+    }
 }
